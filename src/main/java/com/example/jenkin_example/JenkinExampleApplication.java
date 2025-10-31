a5eefa1c9ef3100056385ac19b081fa78a18a9b1
Tinotenda Ruzane <tinoruzane@gmail.com>
2025-10-31 16:04:39 +0200

    first commit

---
+++ src/main/java/com/example/jenkin_example/JenkinExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkin_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkin_example/JenkinExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkin_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
